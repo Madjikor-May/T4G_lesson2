db80a54d40a007c93da77a0713951c1235e555cf
Madjikor-May <mmadjikormensah@gmail.com>
2026-03-30 23:55:42 +0000

    adding all new changes

---
+++ src/index.js
@@ -0,0 +1,16 @@
+console.log("Madjikor");
+
+let x = 15
+console.log(x); //15
+x = 10
+console.log(x); //10 
+
+
+let number = 6; //creating a variable and assigning a avalue
+if(number % 2 == 0)//checking the condition in the bracket to see if it met
+   {alert(`${number} is even`) //if condition is met the number is even number
+   console.log(`${number} is even`)
+   alert(number + "is even") }
+else 
+    {alert(`${number} is odd`); //if condition is not met then the variable is odd and 
+}
